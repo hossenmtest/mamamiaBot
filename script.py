e5e349bb1f6d1a636b311d1741c8272d31d9c2c8
hossenmtest <53256684+hossenmtest@users.noreply.github.com>
2019-07-24 15:48:11 +0430

    Update script.py

--- script.py
+++ script.py
@@ -3,4 +3,4 @@ import time
 
 while True:
     time.sleep(3)
-    urllib.request.urlopen("https://api.telegram.org/bot853024173:AAFhW3lWxL1hfECScSHK6LO8Il-MK6vlQ58/sendAnimation?chat_id=-351281788&animation=CgADBAADGgEAAsNxbVF_fzTzFdji4AI")
+    urllib.request.urlopen("https://api.telegram.org/bot853024173:AAFhW3lWxL1hfECScSHK6LO8Il-MK6vlQ58/sendAnimation?chat_id=-1001490904695&animation=CgADBAADGgEAAsNxbVF_fzTzFdji4AI")
