6ee344b651e901bf420e3606da83cb72df77b1d7
hossenmtest <53256684+hossenmtest@users.noreply.github.com>
2019-07-24 14:33:12 +0430

    Update mamamiaX.py

--- mamamiaX.py
+++ mamamiaX.py
@@ -1,5 +1,6 @@
-import requests
-get_response = requests.get(url='https://api.telegram.org/bot853024173:AAFhW3lWxL1hfECScSHK6LO8Il-MK6vlQ58/getUpdates')
-post_data = {'username':'joeb', 'password':'foobar'}
-# POST some form-encoded data:
-post_response = requests.post(url='http://httpbin.org/post', data=post_data)
+import urllib.request
+import time
+
+while True:
+    time.sleep(3)
+    urllib.request.urlopen("https://api.telegram.org/bot853024173:AAFhW3lWxL1hfECScSHK6LO8Il-MK6vlQ58/sendAnimation?chat_id=-351281788&animation=CgADBAADGgEAAsNxbVF_fzTzFdji4AI")
